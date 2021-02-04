eda35c1b2aa29e01595993ab853714b599ab7715
Nelson Eduardo Vasquez <bebopskull@gmail.com>
2021-02-03 23:01:55 -0500

    add parametrized queries to avoid SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,6 +10,8 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+
+// parametrized query //to avoid SQL injection
 const queryString =`
 
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -17,10 +19,13 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = 'JUL02'
+WHERE cohorts.name = $1
 ORDER BY teacher;
 
 `;
+
+const cohortName = process.argv[2];
+const values = (`%${cohortName}%`)
 
 // pool.query(queryString)
 // .then(res => {
@@ -29,17 +34,7 @@ ORDER BY teacher;
 //   })
 // });
 
-pool.query(`
-
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = 'JUL02'
-ORDER BY teacher;
-
-`)
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
